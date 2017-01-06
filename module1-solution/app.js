49a2643a5e0bccf409c010afca668292e347f1d1
Pushkar Maid <pushkarmaid@Pushkars-MacBook-Pro.local>
2017-01-05 22:10:29 -0800

    Solution 1

---
+++ module1-solution/app.js
@@ -0,0 +1,24 @@
+(function () {
+  'use strict'
+
+  angular.module('firstAssignment', [])
+  .controller('LunchController', function ($scope) {
+    $scope.lunchItems = "";
+
+    $scope.getItems = function () {
+      $scope.message = "";
+      if($scope.lunchItems.trim() == '') {
+        $scope.message = "Error - Please enter data first";
+        return;
+      }
+      var arrayOfStrings = $scope.lunchItems.split(',');
+      if(arrayOfStrings.length > 3) {
+        $scope.message = "Error - Too Much";
+        return;
+      }
+      $scope.message = "Enjoy!";
+    };
+
+});
+
+})();
